b61758ae2c52732d25cab71e954cabc3e13b57bc
martinfzz <forget.l.martin@gmail.com>
2021-06-03 19:08:19 +0200

    Return token in response

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
   
   def render_jsonapi_response(resource)
-    
-      render json: resource
-    
-     
+    render json: {users: resource, token: current_token}
   end
 
   def validation_error(resource)
@@ -20,5 +17,11 @@ class ApplicationController < ActionController::Base
       ]
     }, status: :bad_request
   end
+
+  private
+  def current_token
+    request.env['warden-jwt_auth.token']
+  end
+
 
 end
